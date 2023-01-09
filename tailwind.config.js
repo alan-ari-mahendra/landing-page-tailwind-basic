f3b035c6d3456ac91d556e3379f375348bc92c83
kuuhaku123 <alanpekok8@gmail.com>
2023-01-09 16:36:04 +0700

    Mengupload tugas Landing Page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        add: {
+          dark: '#282A3A',
+          dark1: '#51557E'
+        }
+      },
+    },
+  },
+  plugins: [],
+}
